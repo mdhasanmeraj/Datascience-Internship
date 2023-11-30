6a37467caa4b5784e79b9a37dc07834dfafa76ba
mdhasanmeraj <mdhasanmeraj8@gmail.com>
2023-11-30 15:52:13 +0530

    Initial commit

---
+++ CreatingCSV.py
@@ -0,0 +1,19 @@
+#Create a CSV dataset using Python, Pandas and any public api
+
+#importing pandas and requests for using json
+import pandas as pd
+import requests
+
+# Make a request to the public API
+response = requests.get('https://api.publicapis.org/entries')
+#We only the CSV file for the 'entries' section of the given api
+response.json()['entries']
+data = response.json()['entries']
+
+# Create a DataFrame using the data
+df = pd.DataFrame(data)
+
+# Save the DataFrame to a CSV file
+df.to_csv('data23.csv', index=False, )
+print('CSV file is saved in your device and also print in the terminal')
+print(df)
