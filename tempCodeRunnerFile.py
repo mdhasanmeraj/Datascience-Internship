65b8817f65e00b92c1c9deda1f7b0431d144b66e
mdhasanmeraj <mdhasanmeraj8@gmail.com>
2023-12-15 00:52:12 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-# Load the dataset
-data = pd.read_csv('dataset - netflix1.csv')
-
-# Replace missing values with a specific value or strategy for all columns
-data.fillna(value=0, inplace=True)  # Replace missing values with 0
